f28c42bb819244e1f906ceb68577378aa526b0e7
dilaannkilc <primecodedev01@gmail.com>
2025-11-04 15:16:08 +0200

    Fix duplicate Lenis import causing build failure

--- src/components/Layout/SmoothScroll.jsx
+++ src/components/Layout/SmoothScroll.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import Lenis from 'lenis';
-import Lenis from 'lenis';
 // import 'lenis/dist/lenis.css'; // REMOVED: Managed manually in global CSS to prevent blocking
 
 const SmoothScroll = ({ children }) => {
